40932d618f60a5746cc853215cbc3ad66d081772
SanniM3 <sannimardhiyah@gmail.com>
2021-02-06 15:16:39 +0100

    adding sytem requirements to sidebar

--- sidebars.js
+++ sidebars.js
@@ -1,5 +1,15 @@
 module.exports = {
   someSidebar: {
+  	'System Requirements': ['system-requirements/index',{  		
+  		Hardware: ['system-requirements/hardware/index', 
+  				   'system-requirements/hardware/solaris'],
+  		Software: ['system-requirements/software/index', 
+  				   'system-requirements/software/java',
+  				   'system-requirements/software/perl',  				   
+  				   'system-requirements/software/git',
+  				   ]
+
+  	}],
   	About: ['about/contact-us', 'about/index', 'about/intermine', 'about/privacy-policy', 'about/services'],
     Description: ['doc1'],
   },
